0a548236e5ec5f5037c2ea988978b404de40bf7c
maziyyahjaf <mazi3ah@gmail.com>
2024-10-08 13:40:55 +0800

    first commit

---
+++ src/day05/FileUpload.java
@@ -0,0 +1,16 @@
+package day05;
+
+public class FileUpload {
+
+    // 1. learning to build this model
+    // 2. start word -> generate the next 10 words
+
+    // read all lines, convert to lower case, remove all punctuations except '.'
+    // collections!!
+    
+
+
+
+
+    
+}
